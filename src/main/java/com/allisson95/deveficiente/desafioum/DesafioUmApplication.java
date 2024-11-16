b507d55c292aef6fa0554ed18ca9e1c0d04480ee
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2024-11-16 15:04:29 -0300

    projeto spring boot criado

---
+++ src/main/java/com/allisson95/deveficiente/desafioum/DesafioUmApplication.java
@@ -0,0 +1,13 @@
+package com.allisson95.deveficiente.desafioum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allisson95/deveficiente/desafioum/DesafioUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allisson95.deveficiente.desafioum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
